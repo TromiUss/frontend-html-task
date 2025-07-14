a26827fdc32d91110fb962b0cfc7da0bb613377b
TromiUss <121058447+TromiUss@users.noreply.github.com>
2025-07-14 13:27:20 +0300

    test

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,77 +5,82 @@ import PropTypes from 'prop-types';
 import { SidebarContainer } from './style-comp';
 
 const routes = [
-    { title: 'Home', icon: 'fas-solid fa-house', path: '/' },
-    { title: 'Sales', icon: 'chart-line', path: '/sales' },
-    { title: 'Costs', icon: 'chart-column', path: '/costs' },
-    { title: 'Payments', icon: 'wallet', path: '/payments' },
-    { title: 'Finances', icon: 'chart-pie', path: '/finances' },
-    { title: 'Messages', icon: 'envelope', path: '/messages' },
+  { title: 'Home', icon: 'fas-solid fa-house', path: '/' },
+  { title: 'Sales', icon: 'chart-line', path: '/sales' },
+  { title: 'Costs', icon: 'chart-column', path: '/costs' },
+  { title: 'Payments', icon: 'wallet', path: '/payments' },
+  { title: 'Finances', icon: 'chart-pie', path: '/finances' },
+  { title: 'Messages', icon: 'envelope', path: '/messages' },
 ];
 
 const bottomRoutes = [
-    { title: 'Settings', icon: 'sliders', path: '/settings' },
-    { title: 'Support', icon: 'phone-volume', path: '/support' },
+  { title: 'Settings', icon: 'sliders', path: '/settings' },
+  { title: 'Support', icon: 'phone-volume', path: '/support' },
 ];
 
 const Sidebar = (props) => {
-    const { color } = props;
-    const [isOpened, setIsOpened] = useState(false);
+  const { color } = props;
+  const [isOpened, setIsOpened] = useState(false);
+  const [theme, setTheme] = useState('light');
 
-    const goToRoute = (path) => {
-        console.log(`going to "${path}"`);
-    };
+  const goToRoute = (path) => {
+    console.log(`going to '${path}'`);
+  };
 
-    const toggleSidebar = () => {
-        
-        setIsOpened(v => !v);
-    };
+  const toggleSidebar = () => {
+    setIsOpened((v) => !v);
+  };
 
-    return (
-        <SidebarContainer $isOpened={isOpened}>
-            <div>
-                <img src={ logo } alt="TensorFlow logo"/>
-                <span className="sidebar__title">TensorFlow</span>
-                <div onClick={ toggleSidebar }>
-                    <FontAwesomeIcon icon={ isOpened ? 'angle-left' : 'angle-right' }/>
-                </div>
-            </div>
-            <div>
-                {
-                    routes.map(route => (
-                        <div
-                            key={ route.title }
-                            onClick={() => {
-                                goToRoute(route.path);
-                            }}
-                        >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span className="sidebar__title">{ route.title }</span>
-                        </div>
-                    ))
-                }
-            </div>
-            <div>
-                {
-                    bottomRoutes.map(route => (
-                        <div
-                            key={ route.title }
-                            onClick={() => {
-                                goToRoute(route.path);
-                            }}
-                        >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span className="sidebar__title">{ route.title }</span>
-                        </div>
-                    ))
-                }
-            </div>
-        </SidebarContainer>
-    );
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <SidebarContainer $isOpened={isOpened} $theme={theme}>
+      <div>
+        <img src={logo} alt='TensorFlow logo' />
+        <span className='sidebar__title'>TensorFlow</span>
+        <div onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={isOpened ? 'angle-left' : 'angle-right'} />
+        </div>
+      </div>
+      <div>
+        {routes.map((route) => (
+          <div
+            key={route.title}
+            onClick={() => {
+              goToRoute(route.path);
+            }}
+          >
+            <FontAwesomeIcon icon={route.icon} />
+            <span className='sidebar__title'>{route.title}</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        {bottomRoutes.map((route) => (
+          <div
+            key={route.title}
+            onClick={() => {
+              goToRoute(route.path);
+            }}
+          >
+            <FontAwesomeIcon icon={route.icon} />
+            <span className='sidebar__title'>{route.title}</span>
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={toggleTheme}
+      >
+        {theme === 'light' ? 'Dark' : 'Light'}
+      </button>
+    </SidebarContainer>
+  );
 };
 
 Sidebar.propTypes = {
-    color: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default Sidebar;
--- src/components/Sidebar/style-comp.tsx
+++ src/components/Sidebar/style-comp.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
-export const SidebarContainer = styled.div<{ $isOpened: boolean }>`
-  background-color: var(--color-sidebar-background-light-default);
-  color: var(--color-text-light-default);
+export const SidebarContainer = styled.div<{
+  $isOpened: boolean;
+  $theme: "light" | "dark";
+}>`
+  background-color: var(
+    --color-sidebar-background-${(props) => props.$theme}-default
+  );
+  color: var(--color-text-${(props) => props.$theme}-default);
   width: ${(props) => (props.$isOpened ? "200px" : "70px")};
   height: 600px;
   padding: 10px 0;
@@ -14,32 +19,32 @@ export const SidebarContainer = styled.div<{ $isOpened: boolean }>`
   transition: width 0.5s ease;
 
   & > div {
-    flex-shrink: 0;
-    margin-left: ${(props) => (props.$isOpened ? '30px' : '0')};
+    margin-left: ${(props) => (props.$isOpened ? "25px" : "0")};
+    transition: margin-left 0.5s ease;
   }
 
   .sidebar__title {
     opacity: ${(props) => (props.$isOpened ? 1 : 0)};
     visibility: ${(props) => (props.$isOpened ? "visible" : "hidden")};
     margin-left: ${(props) => (props.$isOpened ? "10px" : "0")};
-    transition: 
-      opacity ${props => props.$isOpened ? '1.5s' : '0.2s'} ease,
-      visibility ${props => props.$isOpened ? '1.5s' : '0.2s'} ease,
-      margin-left ${props => props.$isOpened ? '1.5s' : '0.4s'} ease;
-    position: ${(props) => (props.$isOpened ? "static" : "absolute")};
+    transition: opacity ${(props) => (props.$isOpened ? "1.0s" : "0.2s")} ease,
+      visibility ${(props) => (props.$isOpened ? "1.0s" : "0.2s")} ease,
+      margin-left ${(props) => (props.$isOpened ? "1.0s" : "0.4s")} ease;
+    position: ${(props) => (props.$isOpened ? "absolute" : "absolute")};
+  }
+
+  & > div:first-child .sidebar__title {
+    left: 30px;
   }
 
   & > div:first-child {
     display: flex;
     align-items: center;
-    justify-content: ${(props) => (props.$isOpened ? "space-between" : "center")};
+    justify-content: ${(props) => (props.$isOpened ? "space-between" : "left")};
+    margin-left: ${(props) => (props.$isOpened ? "20px" : "20px")};
     position: relative;
     height: 50px;
     transition: all 0.5s ease;
-  }
-
-  & > div:first-child {
-    margin-left: ${(props) => (props.$isOpened ? "20px" : "0")};
   }
 
   & > div:first-child img {
@@ -51,42 +56,52 @@ export const SidebarContainer = styled.div<{ $isOpened: boolean }>`
 
   & > div:first-child > div {
     position: ${(props) => (props.$isOpened ? "static" : "absolute")};
-    right: ${(props) => (props.$isOpened ? "10px" : "-10px")};
+    right: ${(props) => (props.$isOpened ? "10px" : "-30px")};
     top: 10px;
     cursor: pointer;
     z-index: 10;
-    transition: 
-      right 0.4s ease,
-      opacity 0.4s ease,
-      transform 0.4s ease;
+    transition: right 0.4s ease, opacity 0.4s ease, transform 0.4s ease;
+    width: 20px;
+    text-align: center;
   }
 
-  & > div:nth-child(2) {
+  & > div:not(:first-child) {
     margin-top: 30px;
-    margin-left: ${(props) => (props.$isOpened ? "30px" : "10px")};
     display: flex;
-    width: ${(props) => (props.$isOpened ? "auto" : "50px")};
     flex-direction: column;
-    align-items: ${(props) => (props.$isOpened ? "flex-start" : "center")};
     transition: margin 0.4s ease;
   }
 
-  & > div:nth-child(2) > div {
-    margin: ${(props) => (props.$isOpened ? "10px 0" : "10px 0")};
-    text-align: center;
+  & > div:not(:first-child) > div {
+    margin: ${(props) => (props.$isOpened ? "10px 0" : "10px 25px")};
     transition: margin 0.4s ease;
   }
 
   & > div:last-child {
     margin-top: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: ${(props) => (props.$isOpened ? "flex-start" : "center")};
   }
 
-  & > div:last-child > div {
-    margin: 20px 0;
-    text-align: center;
-    transition: margin 0.4s ease;
+  & > div > div:hover {
+    background-color: var(--color-button-background-${(props) => props.$theme}-hover);
+    color: var(--color-text-${(props) => props.$theme}-hover);
+  }
+
+  & > div > div:active,
+  & > div > div:focus {
+    color: var(--color-text-${(props) => props.$theme}-active);
+    font-weight: bold;
+  }
+
+  button {
+    margin: 0 auto;
+    width: 50px;
+    height: 30px;
+    border-radius: 5px;
+    cursor: pointer;
+    position: relative;
+    display: inline-block;
+    box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
+      7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+    outline: none;
   }
 `;
